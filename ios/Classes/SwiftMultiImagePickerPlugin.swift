e2431e45fbb8fc9a80c175a765ab6d6e17484ac9
Noban Hasan <Nobanhasan@gmail.com>
2020-10-20 13:24:38 +0600

    test push

--- ios/Classes/SwiftMultiImagePickerPlugin.swift
+++ ios/Classes/SwiftMultiImagePickerPlugin.swift
@@ -65,7 +65,7 @@ public class SwiftMultiImagePickerPlugin: NSObject, FlutterPlugin {
             
             if #available(iOS 13.0, *) {
                 // Disables iOS 13 swipe to dismiss - to force user to press cancel or done.
-                vc.isModalInPresentation = true
+                vc.isModalInPresentation = false
             }
             let arguments = call.arguments as! Dictionary<String, AnyObject>
             let maxImages = arguments["maxImages"] as! Int
